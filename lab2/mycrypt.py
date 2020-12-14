6bdb2eb473afb4db90be5c12a7895df7eae6229e
mkoivula18 <44393512+mkoivula18@users.noreply.github.com>
2020-12-14 16:32:26 +0200

    Fixed versions

--- lab2/mycrypt.py
+++ lab2/mycrypt.py
@@ -1,6 +1,9 @@
 import codecs
+import re
+
 
 def encode(s):
+    
     if not isinstance(s,str):
         raise TypeError
     origlen = len(s)
@@ -8,17 +11,46 @@ def encode(s):
     digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
     if len(s) > 1000:
         raise ValueError
+    s = s.ljust(1000, "p")
+    for c in s:
+        
+        if c in digitmapping:
+            crypted+=digitmapping[c]
+        elif c.isalpha():
+            if not re.match("[A-Za-z0-9_-]", c):
+                raise ValueError
+            elif c.islower():
+                c=c.upper()
+            # Rot13 the character for maximum security
+            crypted+=codecs.encode(c,'rot13')
+        elif c == " ":
+            c.replace(" ", "")
+        else:
+            raise ValueError
+
+    return crypted[:origlen]
+
+def decode(s):
+    if not isinstance(s,str):
+        raise TypeError
+    #if len(s) > 1000:
+    #    raise ValueError
+    #s = s.ljust(1000)
+    origlen = len(s)
+    crypted = ""
+    digitmapping = dict(zip('1234567890!"#€%&/()=','!"#€%&/()=1234567890'))
+    if len(s) > 1000:
+        raise ValueError
     for c in s:
         if c.isalpha():
-            if c.islower():
-                c=c.upper()
+            if c.isupper():
+                c=c.lower()
             # Rot13 the character for maximum security
             crypted+=codecs.encode(c,'rot13')
         elif c in digitmapping:
           crypted+=digitmapping[c]
+        else:
+            raise ValueError
 
     return crypted
-
-def decode(s):
-    return s
 
